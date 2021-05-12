4de238cfb8777fcd202ecc17d854a724347ffbdf
yohanamorenosandoval@gmail.com <yohanamorenosandoval@gmail.com>
2021-05-12 11:11:00 -0500

    Boilerplate inicial

--- src/data.js
+++ src/data.js
@@ -1,38 +1,12 @@
 // estas funciones son de ejemplo
 
-//export const example = () => {
- // return 'example';
+export const example = () => {
+  return 'example';
+};
+
+export const anotherExample = () => {
+  return 'OMG';
+};
 
 
- export function sortData(dataPokemones, sortOrdenar){
-
-  const ordenarPokemonName = dataPokemones.sort((a,b) => {
-  if(a.name.toLowerCase()<b.name.toLowerCase()) return -1;
-  if(a.name.toLowerCase()>b.name.toLowerCase()) return 1;
-  return 0;
-  
-});
-
-const ordenarPokemonNum = dataPokemones.sort((a,b) => {
-  if(a.num<b.num) return -1;
-  if(a.num>b.num) return 1;
-  return 0;
-});
-
-if (sortOrdenar === "Z-A"){
-  return result.reverse();
-}
-  else {
-  return result;
-}
-
-//console.log(OrdenarPokemonNum);
-
-//funcion (filtrar)
-/*export function filtrarGeneration(dataPokemones,userCondition){
- const result = data.filtrar(d=> d.status === userCondition);
- return result;
-
-}*/
-
-
+ 
--- src/main.js
+++ src/main.js
@@ -1,33 +1,6 @@
-//import { sortData } from './data.js';
-
-
+import { example } from './data.js';
+// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
+// import data from './data/rickandmorty/rickandmorty.js';
 
-
-//console.log(example, data);
-const dataPokemones = data.pokemon;
-
-function capturar() {
-    console.log(dataPokemones)
-    const ContenedorTarjetas = document.getElementById("Tarjetas");
-    for (let i = 0; i < dataPokemones.length; i++) {
-        console.log(
-            dataPokemones[i].num, dataPokemones[i].name, dataPokemones[i].img);
-        // var element = document.createElement(tagName, [options]);
-        const numeroPokemon = document.createElement("p");
-        numeroPokemon.innerHTML = dataPokemones[i].num;
-        document.getElementById("Tarjetas").appendChild(numeroPokemon);
-
-        const nombrePokemon = document.createElement("h2");
-        nombrePokemon.innerHTML = dataPokemones[i].name;
-        ContenedorTarjetas.appendChild(nombrePokemon);
-
-        const imagenPokemon = document.createElement("img");
-        imagenPokemon.setAttribute("src", dataPokemones[i].img);
-        ContenedorTarjetas.appendChild(imagenPokemon);
-
-
-    }
-}
-
-capturar(dataPokemones)
+console.log(example, data);
