95c8c908672c483d27e601b71e0f50f10e239fcd
yohanamorenosandoval@gmail.com <yohanamorenosandoval@gmail.com>
2021-05-18 04:22:37 -0500

    visualizar pokemones

--- src/data.js
+++ src/data.js
@@ -1,14 +1,37 @@
-// estas funciones son de ejemplo
+//export function sortData(dataPokemones,sortOrdenar)
 
-/*export const example = () => {
-  return 'example';
-};
+export function sortData(dataPokemones, sortOrdenar){
 
-export const anotherExample = () => {
-  return 'OMG';
-};*/
+  const ordenarPokemonName = dataPokemones.sort((a,b) => {
+  if(a.name.toLowerCase()<b.name.toLowerCase()) return -1;
+  if(a.name.toLowerCase()>b.name.toLowerCase()) return 1;
+  return 0;
 
-export function sortData(dataPokemones,sortOrdenar)
+  
+ 
+});
 
+/*const ordenarPokemonNumero = dataPokemones.sort((a,b) => {
+  if(a.num<b.num) return -1;
+  if(a.num>b.num) return 1;
+  return 0;
+});
+
+/*if (sortOrdenar === "Z-A"){
+  return dataPokemones.reverse();
+}
+  else {
+  return dataPokemones;
+}*/
+
+console.log(OrdenarPokemonNum);
+
+//funcion (filtrar)
+/*export function filtrarTypo(dataPokemones,userCondition){
+ const result = data.filtrar(d=> d.status === userCondition);
+ return dataPokemones;
+}
+
+}*/
     
 
--- src/main.js
+++ src/main.js
@@ -1,37 +1,39 @@
-//import { sortData} from './data.js';
+//import { example } from './data.js';
 
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-const dataPokemones = data . pokemon;
 
-console.log(dataPokemones)
+const dataPokemones = data.pokemon;
 
-//const items = document.getElementById("items")
-//const templateCard = document.getElementById("template-card").content
-document.getElementById ("card"). innerHTML =
-dataPokemones. num + dataPokemones. name + dataPokemones.img
+const poke_container = document.getElementById('poke_container');
+const pokemons_number = 251;
 
-  
+
+const contenedorTarjetas = document.getElementById("tarjetas");
+for (let i = 0; i < dataPokemones.length; i++) {
+  //console.log(dataPokemones[i].num, dataPokemones[i].name, dataPokemones[i].img, dataPokemones[i].type)
+    // var element = document.createElement(tagName, [options]);
+    const numeroPokemon = document.createElement("p");
+    numeroPokemon.innerHTML = dataPokemones[i].num;
+    document.getElementById("tarjetas").appendChild(numeroPokemon);
+
+    const nombrePokemon = document.createElement("h2");
+    nombrePokemon.innerHTML = dataPokemones[i].name;
+    contenedorTarjetas.appendChild(nombrePokemon);
+
+    const imagenPokemon = document.createElement("img");
+    imagenPokemon.setAttribute("src",dataPokemones[i].img);
+    contenedorTarjetas.appendChild(imagenPokemon);
+
+    const typePokemon = document.createElement("p");
+    typePokemon.innerHTML = dataPokemones[i].type[0];
+    contenedorTarjetas.appendChild(typePokemon);
+
     
-    const contenedorCards = document.getElementById("card");
-    for (let i = 0; i < dataPokemones.length; i++) {
-        console.log(dataPokemones[i].num, dataPokemones[i].name, dataPokemones[i].img)
-        // var element = document.createElement(tagName, [options]);
-        const numPokemon = document.createElement("p");
-        numPokemon.innerHTML = dataPokemones[i].num;
-        document.getElementById("card").appendChild(numPokemon);
+}
 
-        const namePokemon = document.createElement("h2");
-        namePokemon.innerHTML = dataPokemones[i].name;
-        document.getElementById("card").appendChild(namePokemon);
-
-        const imgPokemon = document.createElement("img");
-        imgPokemon.innerHTML = dataPokemones[i].img;
-        document.getElementById("card").appendChild(imgPokemon);
-    }
-    
-
-    console.log(data)
-
-
+/*const contenedorPokemones = document.createElement("div");
+contenedorPokemones.innerHTML = dataPokemones;
+const id = document.getElementById("tarjetas");
+const pokemon = getElementById("pokemon");
+pokemon.InnerText = tarjetas.InnerText;*/
